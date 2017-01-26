28117bf3fc5ebc7a14bed4143d0c3e39b201b3d1
Akshay <akshay.perfect101@gmail.com>
2017-01-27 00:12:38 +0530

    Hibernate Connection added

--- agent-server/agent-app/src/main/java/com/ayursinfotech/agent/dao/impl/AgentDAOImpl.java
+++ agent-server/agent-app/src/main/java/com/ayursinfotech/agent/dao/impl/AgentDAOImpl.java
@@ -1,6 +1,9 @@
 package com.ayursinfotech.agent.dao.impl;
 
 import org.apache.log4j.Logger;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.ayursinfotech.agent.dao.AgentDAO;
@@ -10,9 +13,13 @@ public class AgentDAOImpl implements AgentDAO {
 
 	private static final Logger LOGGER = Logger.getLogger(AgentDAOImpl.class);
 
+	@Autowired
+	private SessionFactory sessionFactory;
+
 	@Override
 	public Boolean ping() throws Exception {
 		LOGGER.info("start executing ping");
+		Session session = sessionFactory.getCurrentSession();
 		Boolean result = true;
 		LOGGER.info("end executing ping");
 		return result;
